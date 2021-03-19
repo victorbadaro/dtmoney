a076f9d76f40392c1ddb9bbe6958afa70989b4fd
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2021-03-18 23:15:59 -0300

    feat: add Dashboard, Header, Summary and TransactionsTable components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { Header } from "./components/Header";
+import { Dashboard } from "./components/Dashboard";
+
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <>
+      <Header />
+      <Dashboard />
       <GlobalStyle />
-    </div>
+    </>
   );
 }
