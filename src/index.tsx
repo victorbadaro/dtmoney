1a16abbedb8fea0662e600b42fc91d2c6fe3f4e9
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2021-03-20 00:15:38 -0300

    chore: config miragejs fake api

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,19 @@ import { App } from './App';
 createServer({
   routes() {
     this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amout: 400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date()
+        }
+      ];
+    })
   }
 });
 
