41fbb3427cbddeeda90735933bfe63f1278847a4
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2021-03-21 18:44:34 -0300

    chore: useTransactions hook

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
 import { NewTransactionModal } from './components/NewTransactionModal';
 
+import { TransactionsProvider } from './hooks/useTransactions';
+
 import { GlobalStyle } from './styles/global';
 
 export function App() {
@@ -17,7 +19,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
       <NewTransactionModal
@@ -25,6 +27,6 @@ export function App() {
         onRequestClose={handleCloseNewTransactionModal}
       />
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
